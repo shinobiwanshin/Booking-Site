b879460cae1e5ee43f55bfc4d3569e366cd300a0
Amitabha Nath <supersaiyan2k03@gmail.com>
2025-10-06 05:02:00 +0530

    changed some part in qr validation

--- frontend/src/components/nav-bar.tsx
+++ frontend/src/components/nav-bar.tsx
@@ -21,7 +21,11 @@ const NavBar: React.FC = () => {
       <div className="container mx-auto p-4">
         <div className="flex justify-between items-center">
           <div className="flex gap-10 md:gap-20 items-center">
-            <h1 className="text-xl font-bold">Event Ticket Platform</h1>
+            <h1 className="text-xl font-bold">
+              <Link to="/" className="text-xl font-bold hover:underline">
+                Event Ticket Platform
+              </Link>
+            </h1>
             <div className="text-gray-300 flex gap-8">
               {isOrganizer && <Link to="/dashboard/events">Events</Link>}
               <Link to="/dashboard/tickets">Tickets</Link>
--- frontend/src/pages/dashboard-validate-qr-page.tsx
+++ frontend/src/pages/dashboard-validate-qr-page.tsx
@@ -2,6 +2,8 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import { Scanner } from "@yudiel/react-qr-scanner";
+import NavBar from "@/components/nav-bar";
+import React from "react";
 import {
   TicketValidationMethod,
   TicketValidationStatus,
@@ -57,86 +59,92 @@ const DashboardValidateQrPage: React.FC = () => {
   }
 
   return (
-    <div className="min-h-screen bg-black text-white flex justify-center items-center">
-      <div
-        className="border border-gray-400 max-w-sm
+    <>
+      <NavBar />
+      <div className="min-h-screen bg-black text-white flex justify-center items-center">
+        <div
+          className="border border-gray-400 max-w-sm
 w-full p-4"
-      >
-        {error && (
-          <div className="min-h-screen bg-black text-white">
-            <Alert variant="destructive" className="bg-gray-900 border-red-700">
-              <AlertCircle className="h-4 w-4" />
-              <AlertTitle>Error</AlertTitle>
-              <AlertDescription>{error}</AlertDescription>
-            </Alert>
-          </div>
-        )}
-        {/* Scanner Viewport */}
-        <div className="rounded-lg overflow-hidden mx-auto mb-8 relative">
-          <Scanner
-            key={`scanner-${data}-${validationStatus}`}
-            onScan={(result) => {
-              if (result) {
-                const qrCodeId = result[0].rawValue;
-                setData(qrCodeId);
-                handleValidate(qrCodeId, TicketValidationMethod.QR_SCAN);
-              }
-            }}
-            onError={handleError}
-          />
-
-          {validationStatus && (
-            <div className="absolute inset-0 flex items-center justify-center">
-              {validationStatus === TicketValidationStatus.VALID ? (
-                <div className="bg-green-500 rounded-full p-4">
-                  <Check className="w-20 h-20" />
-                </div>
-              ) : (
-                <div className="bg-red-500 rounded-full p-4">
-                  <X className="w-20 h-20" />
-                </div>
-              )}
+        >
+          {error && (
+            <div className="min-h-screen bg-black text-white">
+              <Alert
+                variant="destructive"
+                className="bg-gray-900 border-red-700"
+              >
+                <AlertCircle className="h-4 w-4" />
+                <AlertTitle>Error</AlertTitle>
+                <AlertDescription>{error}</AlertDescription>
+              </Alert>
             </div>
           )}
-        </div>
-
-        {isManual ? (
-          <div className="pb-8">
-            <Input
-              className="w-full text-white text-lg mb-8"
-              onChange={(e) => setData(e.target.value)}
+          {/* Scanner Viewport */}
+          <div className="rounded-lg overflow-hidden mx-auto mb-8 relative">
+            <Scanner
+              key={`scanner-${data}-${validationStatus}`}
+              onScan={(result) => {
+                if (result) {
+                  const qrCodeId = result[0].rawValue;
+                  setData(qrCodeId);
+                  handleValidate(qrCodeId, TicketValidationMethod.QR_SCAN);
+                }
+              }}
+              onError={handleError}
             />
-            <Button
-              className="bg-purple-500 w-full h-[80px] hover:bg-purple-800"
-              onClick={() =>
-                handleValidate(data || "", TicketValidationMethod.MANUAL)
-              }
-            >
-              Submit
-            </Button>
-          </div>
-        ) : (
-          <div>
-            <div className="border-white border-2 h-12 rounded-md font-mono flex justify-center items-center">
-              <span>{data || "Scan for Result"}</span>
-            </div>
-            <Button
-              className="bg-gray-900 hover:bg-gray-600 border-gray-500 border-2 w-full h-[80px] text-xl my-8"
-              onClick={() => setIsManual(true)}
-            >
-              Manual
-            </Button>
-          </div>
-        )}
 
-        <Button
-          className="bg-gray-500 hover:bg-gray-800 w-full h-[80px] text-xl my-8"
-          onClick={handleReset}
-        >
-          Reset
-        </Button>
+            {validationStatus && (
+              <div className="absolute inset-0 flex items-center justify-center">
+                {validationStatus === TicketValidationStatus.VALID ? (
+                  <div className="bg-green-500 rounded-full p-4">
+                    <Check className="w-20 h-20" />
+                  </div>
+                ) : (
+                  <div className="bg-red-500 rounded-full p-4">
+                    <X className="w-20 h-20" />
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
+
+          {isManual ? (
+            <div className="pb-8">
+              <Input
+                className="w-full text-white text-lg mb-8"
+                onChange={(e) => setData(e.target.value)}
+              />
+              <Button
+                className="bg-purple-500 w-full h-[80px] hover:bg-purple-800"
+                onClick={() =>
+                  handleValidate(data || "", TicketValidationMethod.MANUAL)
+                }
+              >
+                Submit
+              </Button>
+            </div>
+          ) : (
+            <div>
+              <div className="border-white border-2 h-12 rounded-md font-mono flex justify-center items-center">
+                <span>{data || "Scan for Result"}</span>
+              </div>
+              <Button
+                className="bg-gray-900 hover:bg-gray-600 border-gray-500 border-2 w-full h-[80px] text-xl my-8"
+                onClick={() => setIsManual(true)}
+              >
+                Manual
+              </Button>
+            </div>
+          )}
+
+          <Button
+            className="bg-gray-500 hover:bg-gray-800 w-full h-[80px] text-xl my-8"
+            onClick={handleReset}
+          >
+            Reset
+          </Button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
